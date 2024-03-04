1af850f7e422d8550f3d8bbaa8fd829996761093
Tiago Raitz <60758361+raitztiago1@users.noreply.github.com>
2024-03-03 21:43:59 -0300

    Start Commit

---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import { Header } from "@/components/header"
+import { View, Text } from "react-native"
+
+export default function Home() {
+    return (
+        <View>
+            <Header title="Faça seu pedido" />
+        </View>
+    )
+}
---
+++ src/components/header.tsx
@@ -0,0 +1,15 @@
+import { Image, Text, View } from "react-native";
+
+type HeaderProps = {
+    title: string
+}
+export function Header({ title }: HeaderProps) {
+    return (
+        <View className="flex-row items-center border-b border-slate-700 pb-5 mx-5">
+            <View className="flex-1">
+                <Image source={require("@/assets/logo.png")} className="h-6 w-32" />
+                <Text className="text-white text-xl font-heading mt-2">{title}</Text>
+            </View>
+        </View>
+    )
+}
