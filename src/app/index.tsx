7c5004419ecc23ae046c50d7a138f2dab5a2934b
Tiago Raitz <60758361+raitztiago1@users.noreply.github.com>
2024-03-04 19:23:32 -0300

    Primeiro botão/componente

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,3 +1,4 @@
+import { CategoryButton } from "@/components/category-button"
 import { Header } from "@/components/header"
 import { View, Text } from "react-native"
 
@@ -5,6 +6,13 @@ export default function Home() {
     return (
         <View>
             <Header title="Faça seu pedido" />
+
+            <View className="flex-row gap-4">
+                <CategoryButton title="Lanche do dia" isSelected />
+                <CategoryButton title="Lanche aa" />
+                <CategoryButton title="Lanche bb" />
+            </View>
         </View>
+
     )
 }
